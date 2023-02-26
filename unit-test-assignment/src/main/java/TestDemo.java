1b14e4269cb1c6422b5f4f04bf3d41a1888bd683
Michael Duran <duran122410@gmail.com>
2023-02-25 22:32:56 -0500

    First Commit

---
+++ unit-test-assignment/src/main/java/TestDemo.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+
+import com.google.common.annotations.VisibleForTesting;
+
+public class TestDemo {
+
+	public int addPositive(int a, int b) {
+		if (a > 0 && b > 0) {
+			return (a + b);
+		} else {
+			throw new IllegalArgumentException("Both parameters must be positive!");
+		}
+	}
+
+	@VisibleForTesting
+	int randomNumberSquared() {
+		int squaredNumber = getRandomInt();
+		return (squaredNumber * squaredNumber);
+	}
+
+	@VisibleForTesting
+	int getRandomInt() {
+		Random random = new Random();
+		return random.nextInt(10) + 1;
+	}
+
+}
